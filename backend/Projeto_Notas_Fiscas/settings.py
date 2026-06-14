142cee164441108906bdb0335a3778bf562e2e2d
Joao <joaopedrolima0115@gmail.com>
2026-06-14 19:17:39 -0300

    fix(backend): adiciona SECURE_PROXY_SSL_HEADER para corrigir o proxy do render

--- backend/Projeto_Notas_Fiscas/settings.py
+++ backend/Projeto_Notas_Fiscas/settings.py
@@ -168,3 +168,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 50,
 }
+
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
